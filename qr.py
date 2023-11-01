0ffdba48d53044fe60c505713875e54d1d169fdd
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-11-01 12:30:29 +0800

    chnages

--- qr.py
+++ qr.py
@@ -1,3 +1,7 @@
 import qrcode
 
-features = qrcode.QRCode(version=1, box_size=40 )
+features = qrcode.QRCode(version=1, box_size=40, border=5)
+features.add_data('https://www.instagram.com/ar/261938039613412/')
+features.make(fit=True)
+generate_image = features.make_image(fill_color='black')
+generate_image.save('Reconqr.png')
